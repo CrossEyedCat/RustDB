beeb0037f7332681a483bc2aa37cee08c91e225d
CrossEyedCat <CrossEyedCat@users.noreply.github.com>
2026-03-17 10:15:06 +0000

    deploy: 5454352a59b9e0392e76ac678c8746032b988108

--- docs/trait.impl/clap_builder/derive/trait.Parser.js
+++ docs/trait.impl/clap_builder/derive/trait.Parser.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["rustdb",[["impl Parser for <a class=\"struct\" href=\"rustdb/cli/struct.Cli.html\" title=\"struct rustdb::cli::Cli\">Cli</a>"]]]]);
+    const implementors = Object.fromEntries([["rustdb",[["impl Parser for <a class=\"struct\" href=\"rustdb/cli/struct.Cli.html\" title=\"struct rustdb::cli::Cli\">Cli</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[130]}
+//{"start":59,"fragment_lengths":[132]}
--- docs/trait.impl/clap_builder/derive/trait.Subcommand.js
+++ docs/trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["rustdb",[["impl Subcommand for <a class=\"enum\" href=\"rustdb/cli/enum.Commands.html\" title=\"enum rustdb::cli::Commands\">Commands</a>"],["impl Subcommand for <a class=\"enum\" href=\"rustdb/cli/enum.LanguageCommands.html\" title=\"enum rustdb::cli::LanguageCommands\">LanguageCommands</a>"]]]]);
+    const implementors = Object.fromEntries([["rustdb",[["impl Subcommand for <a class=\"enum\" href=\"rustdb/cli/enum.Commands.html\" title=\"enum rustdb::cli::Commands\">Commands</a>",0],["impl Subcommand for <a class=\"enum\" href=\"rustdb/cli/enum.LanguageCommands.html\" title=\"enum rustdb::cli::LanguageCommands\">LanguageCommands</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[298]}
+//{"start":59,"fragment_lengths":[302]}
